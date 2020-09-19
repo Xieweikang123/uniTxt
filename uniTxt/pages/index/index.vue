13834f7d4dbd104aa26c30fc2d2e1811bf55f0ef
walker <2303061691@qq.com>
2020-09-19 21:52:53 +0800

    init

---
+++ uniTxt/pages/index/index.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="content">
+		<div class="verticalMiddle">
+			<input class="inputStyle" v-model="iptValue"  type="text">
+			<button >确定</button>
+		</div>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				iptValue:'测试文字',
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.inputStyle{
+		    height: 40px;
+		    padding-left: 12px;
+		border:1px solid #007AFF;
+		margin-bottom:38px;
+	}
+	.verticalMiddle{
+		    padding: 0 25px;
+		    margin-top: 40%;
+	}
+	.content {
+	    height: 100%;
+	    position: absolute;
+	    width: 100%;
+	}
+
+ 
+</style>
